f88ada3fa81e12ef1a4c66b006c76514c475f8fe
alex chousmith <alex@ninthlink.com>
2016-10-28 00:32:26 -0700

    Reader View Available via undoing super smashing or something

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,10 +6,8 @@ jQuery(function($){
       topw = 0,
       topc = 0,
       mhei = 0,
-      $banner = $('#top').parent(),
+      $banner = $('#top'),
       $navbar = $banner.next();
-
-  // console.log('and then?');
 
   // http://appelsiini.net/projects/lazyload
   $('img[data-original]').lazyload({
@@ -17,9 +15,7 @@ jQuery(function($){
   });
 
   // smooth scroll catch
-  $('a[href*="#"]').on('navactive', function() {
-    $(this).removeClass('link white-70 hover-blue').addClass('white').siblings('.white').removeClass('white').addClass('link white-70 hover-blue');
-  }).click(function(e) {
+  $('a[href*="#"]').click(function(e) {
     e.preventDefault();
     var whash = $(this).attr('href');
     var $tar = $( whash );
@@ -27,7 +23,11 @@ jQuery(function($){
     if ( $tar.size() > 0 ) {
       // sooth scroll
       var $top = $tar.offset();
-      $top = $top.top - mhei;
+      $top = $top.top;
+      // if scrolling down, dont adjust
+      if ( $top < $(window).scrollTop() ) {
+        $top -= mhei;
+      }
       $('html, body').animate({
         'scrollTop': $top
       }, 800, function() {
@@ -41,48 +41,29 @@ jQuery(function($){
     // navbar height
     mhei = parseInt( $navbar.css('height'), 10 );
     // top of resume section = bottom of banner + navbar height
-    topr = ($('#about').parent().offset()).top - mhei;
+    topr = ($('#about').offset()).top - mhei;
     // console.log('topr take 2: '+ topr2);
     // work / portfolio
-    topw = ($('#work').parent().offset()).top;
+    topw = ($('#work').offset()).top;
     // contact
-    topc = ($('#contact').parent().offset()).top;
+    topc = ($('#contact').offset()).top;
   }).trigger('resize')
   .on('resetnav', function() {
-    $navbar.removeAttr('style').find('nav a:eq(0)').trigger('navactive');
+    $navbar.removeAttr('style');//.find('nav a:eq(0)').trigger('navactive');
     $banner.css('margin-bottom', 0);
   })
   .on('scrollthrottle', function() {
-    // console.log('scrollthrottled : last top  = '+ lastscrolltop +' : new = '+ newscrolltop);
-    if ( newscrolltop <= topr ) {
-      // console.log('reset whash?');
-      // window.location.hash = '';
-    } else {
-      if ( newscrolltop >= topc ) {
-        // window.location.hash = '#contact';
-        $navbar.find('nav a:last').trigger('navactive');
-      } else if ( newscrolltop >= topw ) {
-        // window.location.hash = '#work';
-        $navbar.find('nav a:eq(2)').trigger('navactive');
-      } else {
-        // window.location.hash = '#about';
-        $navbar.find('nav a:eq(1)').trigger('navactive');
-      }
-
+    if ( newscrolltop > topr ) {
       if ( lastscrolltop > newscrolltop ) {
         // scrolled up. check if still below bottom of top navbar
-        // if ( newscrolltop > topr ) {
-          // console.log('scrolldup');
-          if ( !$navbar.is('[style]') ) {
-            $navbar.stop().css({'position': 'fixed', 'top': -1 * mhei });
-            $banner.css('margin-bottom', mhei);
-            $navbar.animate({
-              top: 0
-            }, 200);
-          }
-        // }
+        if ( !$navbar.is('[style]') ) {
+          $navbar.stop().css({'position': 'fixed', 'top': -1 * mhei });
+          $banner.css('margin-bottom', mhei);
+          $navbar.animate({
+            top: 0
+          }, 200);
+        }
       } else {
-        // console.log('scrolled down');
         if ( $navbar.is('[style]') ) {
           $navbar.stop().animate({
             top: -1 * mhei
@@ -96,22 +77,23 @@ jQuery(function($){
   })
   .on('scroll', function(e) {
     newscrolltop = $(this).scrollTop();
+
     clearTimeout( scrollthrottler );
+
     if ( newscrolltop <= topr ) {
       // make sure navvar is un fixed
       $(window).trigger('resetnav');
     }
-    // else {
-      scrollthrottler = setTimeout( function() {
-        $(window).trigger('scrollthrottle');
-      }, 100 );
-    // }
+
+    scrollthrottler = setTimeout( function() {
+      $(window).trigger('scrollthrottle');
+    }, 100 );
   });
 
   // em fx
-  $('#contact').next().next().children('a').each(function() {
+  $('#contact').find('a[title=Email]').each(function() {
     var th = $(this).html();
-    th = th.replace(' [ @ ] ', '@') +'.com';
+    th = th.replace(' [ at ] ', '@') +'.com';
     $(this).attr('href', 'mailto:'+ th).html(th).addClass('link hover-blue');
   });
 
